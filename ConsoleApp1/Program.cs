f8e58df4384e55982b1286af043f2041ca6012e2
sdilsiz <dilsizsalih@gmail.com>
2020-12-12 19:36:41 +0100

    update solution name

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1 {
+    class Program {
+        public static Func<int, string, string> UppercaseString = (str, index) => str + " " + index;
+
+        static void Main(string[] args) {
+
+
+            int[] numbers = { 1, 2, 3, 4 };
+            string[] words = { "one", "two", "three" };
+            //Func<string, string> selector = (first,  second) => first + " " + second;
+            //Func<string, string, string> convertMethod = UppercaseString;
+            
+             
+            // Use delegate instance to call UppercaseString method
+            
+
+            var numbersAndWords = words.Zip(numbers, (first, second) => first + " " + second);
+
+            foreach (var item in numbersAndWords)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+
+
+         
+    }
+}
